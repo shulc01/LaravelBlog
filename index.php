3d5c6272d2cf10a5527a5ef544919198c31a1848
shulc01 <agroruno@gmail.com>
2017-10-06 15:36:20 +0300

    4

---
+++ index.php
@@ -0,0 +1,7 @@
+﻿echo "# LaravelBlog" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:shulc01/LaravelBlog.git
+git push -u origin master
+
