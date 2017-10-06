765c651a70d13d49b12d27bc09fe9c4090e6c173
shulc01 <32567480+shulc01@users.noreply.github.com>
2017-10-06 15:39:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-﻿echo "# LaravelBlog" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:shulc01/LaravelBlog.git
-git push -u origin master
+asfdsdfsafsdf
 
